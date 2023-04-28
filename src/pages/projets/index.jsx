0e2af8558119d307e3279a7dd200fe5d809dbb3a
bibithom <jeremy.darbellay@gmail.com>
2023-04-28 15:28:07 +0200

    transformation de la page de projets en jsx plutôt qu'mdx

---
+++ src/pages/projets/index.jsx
@@ -0,0 +1,10 @@
+export default function Projets() {
+    
+    return (
+        <>
+            <h1> Mes projets finalisés </h1>
+            <p>Introduction projets @TODO</p>
+
+        </>
+    )
+}
