8265087decf7e7c6a9e8dd7c5e67df945080ccf3
bibithom <jeremy.darbellay@gmail.com>
2023-04-28 20:52:22 +0200

    ajout d'un bouton pour changer de mode

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,8 +1,32 @@
 import Link from 'next/link'
 import styles from './layout.module.css'
 
+import { useEffect, useState } from 'react'
 
 export default function Layout({ children }) {
+
+
+    useEffect( () => {
+        if (typeof window !== "undefined") {
+            if (localStorage.getItem('theme') === 'opposite') document.documentElement.classList.add('opposite');
+        }
+    }, [])
+
+    const [ oppositeMode, setOppositeMode ] = useState(false);
+
+    function handleToggleModeButtonClick() {
+
+        document.documentElement.classList.toggle('opposite');
+
+        if (typeof window !== "undefined") {
+            localStorage.getItem('theme') === 'opposite' ? localStorage.removeItem('theme') : localStorage.setItem('theme', 'opposite');
+        }
+
+        setOppositeMode(!oppositeMode);
+
+    }
+
+
     return (
       <>
         <header className={styles.header}>
@@ -10,7 +34,7 @@ export default function Layout({ children }) {
             <nav>
                 <ul>
                     <li className={styles.mainLink}>
-                        <Link href="/cv" className="link">C.V.</Link>
+                        <Link href="/curriculum_vitae" className="link">C.V.</Link>
                     </li>
                     <li className={styles.mainLink}>
                         <Link href="/projets" className="link">Projets</Link>
@@ -19,7 +43,7 @@ export default function Layout({ children }) {
                         <Link href="/competences" className="link">Compétences</Link>
                     </li>
                     <li className={styles.mainLink}>
-                        <button>Color mode</button>
+                        <button onClick={handleToggleModeButtonClick}>Color mode</button>
                     </li>
                 </ul>
             </nav>
@@ -31,7 +55,7 @@ export default function Layout({ children }) {
             <p>© Darbellay Jérémy - 2023</p>
             <ul>
                 <li>
-                    <Link className="link" href="/">contact</Link>
+                    <Link className="link" href="mailto:jeremy.darbellay@gmail.com">contact</Link>
                 </li>
                 <li>
                     <Link className="link" href="/legal_terms">mentions légales</Link>
