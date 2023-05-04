c252c98d8e45933c8041607bc956ef296891db6c
bibithom <jeremy.darbellay@gmail.com>
2023-05-04 16:17:06 +0200

    progression sur les pages mdx (contenu)

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,7 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-
 // error page
 export default function Custom404() {
 
-  const router = useRouter()
-  useEffect(() => {
-    router.push('/404')
-  }, [])
-
-  return <h1>404 - Page Not Found</h1>
+  return <main><h1>404 - Page introuvable</h1><p>Oulah t'es perdu ?</p></main>
   
   }
   
